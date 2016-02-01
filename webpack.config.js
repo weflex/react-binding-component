25de32c5b2a4c7ebee37948be45fb3ff4a53c347
yorkie <yorkiefixer@gmail.com>
2016-02-01 17:53:57 +0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'gian.js',
+    filename: 'react-binding-component.js',
     libraryTarget: 'commonjs2'
   },
   resolve: {
