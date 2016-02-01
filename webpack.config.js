ae3d796c8349f06c48a3a855f29077d062224bb7
yorkie <yorkiefixer@gmail.com>
2016-02-01 17:23:43 +0800

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    path.join(__dirname, 'lib/react-binding-component.js'),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'gian.js',
+    libraryTarget: 'commonjs2'
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ]
+};
